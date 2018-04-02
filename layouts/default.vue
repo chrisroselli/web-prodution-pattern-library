2c121d95c73d752eee2206f977d8b1b411b5bbc9
Chris Roselli <chrisroselli@gmail.com>
2018-04-02 16:32:54 -0400

    push all files

---
+++ layouts/default.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer :mini-variant.sync="miniVariant" :clipped="clipped" v-model="drawer" fixed app>
+      <v-list>
+        <v-list-tile router :to="item.to" :key="i" v-for="(item, i) in items" exact>
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="light-green darken-2" dark fixed app :clipped-left="clipped">
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer color="brown darken-2" dark :fixed="fixed" app>
+      <span>&copy; 2017 Treehouse Internet Group</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        clipped: true,
+        drawer: true,
+        fixed: false,
+        items: [{
+            icon: 'apps',
+            title: 'Introduction',
+            to: '/'
+          },
+          {
+            icon: 'bubble_chart',
+            title: 'Inspire',
+            to: '/inspire'
+          }
+        ],
+        miniVariant: false,
+        right: false,
+        rightDrawer: false,
+        title: 'Treehouse Pattern Library'
+      }
+    }
+  }
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-layout>
+    <v-flex sm12>
+      <v-card>
+        <v-tabs v-model="active" color="brown darken-2" dark slider-color="light-green darken-2">
+          <v-tab>
+            HTML
+          </v-tab>
+          <v-tab-item>
+            <v-card color="light-green lighten-4">
+              <v-card-text>
+                <pre v-highlightjs="html">
+                    <code class="html">
+                    </code>
+                  </pre>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+          <v-tab>
+            CSS
+          </v-tab>
+          <v-tab-item>
+            <v-card color="light-green lighten-4">
+              <v-card-text>
+                <pre v-highlightjs="css">
+                    <code class="css">
+                    </code>
+                  </pre>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+          <v-tab>
+            JS
+          </v-tab>
+          <v-tab-item>
+            <v-card color="light-green lighten-4">
+              <v-card-text>
+                <pre v-highlightjs="js">
+                    <code class="js">
+                    </code>
+                  </pre>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+        </v-tabs>
+      <v-layout pa-5 row justify-space-between>
+      <v-flex xs6>
+
+      </v-flex>
+      <v-flex xs8>
+     <span v-html="component"></span>
+  
+      </v-flex>
+      <v-flex xs6>
+
+      </v-flex>
+    </v-layout>
+
+            
+
+
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  import VueHighlightJS from "vue-highlightjs";
+  import "highlight.js/styles/atom-one-dark.css";
+  export default {
+    data() {
+      return {
+        active: null,
+        html: '<a class="cta-button" href="/free-estimate.html">Schedule A Free Estimate</a>',
+        css: `.cta-button {
+                display: inline-block;
+                background-color: #353535;
+                text-transform: uppercase;
+                padding: 1rem 2rem 1.0625rem 2rem;
+                color: #fff;
+                font-size: 20px;
+                font-weight: bold;
+                position: relative;
+                bottom: 5px;
+                transition: all 0.4s ease 0s;
+                  }`,
+        js: "",
+        component: `
+                <style>
+                  .cta-button {
+                    display: inline-block;
+                    background-color: #353535;
+                    text-transform: uppercase;
+                    padding: 1rem 2rem 1.0625rem 2rem;
+                    color: #fff;
+                    font-size: 20px;
+                    font-weight: bold;
+                    position: relative;
+                    bottom: 5px;
+                    transition: all 0.4s ease 0s;
+                    text-decoration: none;
+                  }
+                </style>
+            <a class="cta-button" href="/free-estimate.html">Schedule A Free Estimate</a>
+            `
+      };
+    },
+    methods: {
+      next() {
+        const active = parseInt(this.active);
+        this.active = (active < 2 ? active + 1 : 0).toString();
+      }
+    }
+  };
+</script>
